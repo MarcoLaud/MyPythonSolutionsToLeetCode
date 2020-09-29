b49a9d05ebb6e43627e57ac667e3a0cf8cedc441
MarcoLaud <info@marcolaudato.com>
2020-09-29 17:46:08 +0200

    Flip And Invert Image

---
+++ FlipAndInvertImage.py
@@ -0,0 +1,20 @@
+#    Given a binary matrix A, we want to flip the image horizontally,
+#    then invert it, and return the resulting image.
+#
+#    To flip an image horizontally means that each row of the image
+#    is reversed.  For example, flipping [1, 1, 0] horizontally
+#    results in [0, 1, 1].
+#
+#    To invert an image means that each 0 is replaced by 1,
+#    and each 1 is replaced by 0. For example,
+#    inverting [0, 1, 1] results in [1, 0, 0].
+
+
+class Solution:
+    def flipAndInvertImage(self, A):
+        return [[(elem + 1) % 2 for elem in row][::-1] for row in A]
+
+
+if __name__ == "__main__":
+    testinput = [[1, 1, 0], [1, 0, 1], [0, 0, 0]]
+    print(Solution.flipAndInvertImage(Solution, testinput))
