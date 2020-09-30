6c00f1489e17b0ddd9388fcfd89c8e914d6fecf7
MarcoLaud <info@marcolaudato.com>
2020-09-30 17:54:24 +0200

    Final Price with Special Discount in Shop

---
+++ FinalPriceWithSpecialDiscountInShop.py
@@ -0,0 +1,25 @@
+#    Given the array prices where prices[i] is the price of the
+#    ith item in a shop. There is a special discount for items in
+#    the shop, if you buy the ith item, then you will receive a discount
+#    equivalent to prices[j] where j is the minimum index such that
+#    j > i and prices[j] <= prices[i], otherwise, you will not receive
+#    any discount at all.
+#
+#    Return an array where the ith element is the final price
+#    you will pay for the ith item of the shop considering
+#    the special discount.
+
+
+class Solution:
+    def finalPrices(self, prices):
+        for ii, price in enumerate(prices[:-1]):
+            for discount in prices[ii+1:]:
+                if price >= discount:
+                    prices[ii] = price - discount
+                    break
+        return prices
+
+
+if __name__ == "__main__":
+    testinput = [8, 4, 6, 2, 3]
+    print(Solution.finalPrices(Solution, testinput))
