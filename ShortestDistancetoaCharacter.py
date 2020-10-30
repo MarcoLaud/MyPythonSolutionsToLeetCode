e149ffbe1d7389ba8da251eebd83459fce2de3d4
MarcoLaud <info@marcolaudato.com>
2020-10-30 13:35:18 +0100

    Shortest Distance To A Character

---
+++ ShortestDistancetoaCharacter.py
@@ -0,0 +1,21 @@
+#    Given a string S and a character C, return an array of integers
+#    representing the shortest distance from the character C in the string.
+
+
+class Solution:
+    def shortestToChar(self, S, C):
+        res = []
+        buffer = []
+        indeces = [ii for ii in range(len(S)) if S[ii] == C]
+        for ii in range(len(S)):
+            for jj in map(lambda x: x - ii, indeces):
+                buffer.append(abs(jj))
+            res.append(min(buffer))
+            buffer = []
+        return res
+
+
+if __name__ == "__main__":
+    testinput1 = "loveleetcode"
+    testinput2 = 'e'
+    print(Solution.shortestToChar(Solution, testinput1, testinput2))
